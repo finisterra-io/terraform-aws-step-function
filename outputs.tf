1705449fad153fedeb23f1e26aff580782995e25
Daniel J <daniel@finisterra.io>
2024-02-06 10:10:23 -0500

    feat: initial release

---
+++ outputs.tf
@@ -0,0 +1,20 @@
+# Step Function
+output "state_machine_id" {
+  description = "The ARN of the Step Function"
+  value       = try(aws_sfn_state_machine.this[0].id, "")
+}
+
+output "state_machine_arn" {
+  description = "The ARN of the Step Function"
+  value       = try(aws_sfn_state_machine.this[0].arn, "")
+}
+
+output "state_machine_creation_date" {
+  description = "The date the Step Function was created"
+  value       = try(aws_sfn_state_machine.this[0].creation_date, "")
+}
+
+output "state_machine_status" {
+  description = "The current status of the Step Function"
+  value       = try(aws_sfn_state_machine.this[0].status, "")
+}
